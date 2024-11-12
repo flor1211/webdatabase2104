541bce6491ba9480e4f35e6f48460fcdcc1359ad
flor1211 <florenciomendozarosalesjr@gmail.com>
2024-11-12 12:13:45 +0800

    added a feature for database connection

--- submit.php
+++ submit.php
@@ -11,12 +11,22 @@
     $conn = new mysqli(hostname: $hostname,username: $username,password: $password,database: $dbname);
 
 
+        $sql;
+
 
     if ($conn->connect_error){
         echo "Connection Failed! " . $conn->connect_error;
     } else{
         echo "Connection Successful";
+
+        $sql= "INSERT INTO dbauth(username, password) VALUES ($username, $password)";
+
+        mysqli_query($conn, $sql);
     }
+
+    mysqli_close($conn);
+    
+
 
 
 
